090c1148e2a50916391b91fc31d8807d6c05f980
Tim Shaker <tim@tmshkr.com>
2020-07-21 13:52:25 -0700

    complete merge_in_place stretch

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -6,7 +6,7 @@ def merge(a, b):
 
     # both i and j in their respective range
     while i < len(a) and j < len(b):
-        # append the lesser item first
+        # append the lesser element first
         if a[i] < b[j]:
             merged.append(a[i])
             i += 1
@@ -48,9 +48,48 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 
 
-def merge_in_place(arr, start, mid, end):
-    pass
+def merge_in_place(arr, a, b):
+    i = 0
+    j = 0
+    k = 0
+
+    # both i and j in their respective range
+    while i < len(a) and j < len(b):
+        # append the lesser element first
+        if a[i] < b[j]:
+            arr[k] = a[i]
+            i += 1
+        else:
+            arr[k] = b[j]
+            j += 1
+        k += 1
+
+    # append the rest of the elements once
+    # either i or j are out of range
+    while i < len(a):
+        arr[k] = a[i]
+        i += 1
+        k += 1
+    while j < len(b):
+        arr[k] = b[j]
+        j += 1
+        k += 1
+
+    return arr
 
 
-def merge_sort_in_place(arr, l, r):
-    pass
+def merge_sort_in_place(arr):
+    # BASE CASE: an array of 1 or 0 elements,
+    # which means that it is already sorted
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    # recursively call merge_sort on the
+    # left and right halves of the array
+    left = merge_sort_in_place(arr[:mid])
+    right = merge_sort_in_place(arr[mid:])
+
+    # then merge them
+    return merge_in_place(arr, left, right)
